3666439f6acf2f358e3efe60ee3ba197656f7844
Shutova Anna <90278446+DangerNoodle557@users.noreply.github.com>
2021-10-25 22:04:46 +0500

    Add files via upload

---
+++ задача с комбинацией.pas
@@ -0,0 +1,33 @@
+﻿var x,m,i,j,k,l :integer; a:array[1..3] of integer;
+
+begin
+writeln('введите трехзначное число');
+readln(x);
+if (x>99)and (x<1000) then
+begin
+a[1]:=x div 100;
+a[2]:=(x div 10) mod 10;
+a[3]:= x mod 10;
+end
+else writeln('вы ввели не трехзначное число');
+ 
+m:=0;
+
+for i:=1 to 3 do
+for j:=1 to 3 do
+for k:=1 to 3 do
+if (i<>j) and (i<>k) and (j<>k) then
+writeln (a[i],a[j],a[k]);
+
+for i:=1 to 3 do
+for j:=1 to 3 do
+for k:=1 to 3 do
+if (i<>j) and (i<>k) and (j<>k) then
+begin
+l:=a[i]*100 + a[j]*10 + a[k];
+if l>m then
+m:=l;
+end;
+
+writeln ('max = ',m);
+end.
---
+++ угадайка.pas
@@ -0,0 +1,25 @@
+﻿var a,b,n:integer;
+
+begin
+n:=0;
+a:=random(99)+1;
+writeln('угадайте число от 1 до 100, у вас есть 6 попыток');
+
+begin
+
+while n<6 do
+begin
+readln(b);
+if b=a then
+writeln('вы угадали число!')
+else 
+begin 
+n:=n+1;
+if a>b then writeln('попробуйте ввести число побольше')
+else writeln('попробуйте ввести число поменьше');
+end;
+end;
+end;
+writeln('вы проиграли',', загаданное число - ', a);
+
+end.
