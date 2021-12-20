2825f7baf26615cf405bafdff623007083b44a93
Shutova Anna <90278446+DangerNoodle557@users.noreply.github.com>
2021-12-21 03:47:41 +0500

    Create 3_1.cpp

---
+++ algoritm/3_1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+    int n;
+    n = 5;
+    int tmp;
+    int arr[5] = { 36, 99, 3, 68, 1 };
+    for (int j = 1; j < n; j++) 
+    {
+        tmp = arr[j];
+        int i = j - 1;
+        while (i >= 0 && arr[i] > tmp) 
+        {
+            arr[i + 1] = arr[i];
+            i--;
+        }
+        arr[i + 1] = tmp;
+    }
+    for (int j = 0; j < n; j++) 
+    {
+        cout << arr[j] << " ";
+    }
+    return 0;
+}
